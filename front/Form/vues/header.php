a71daba4cc4eb4ca9869e7c7c8bdb47b7d0203f2
Abshol <abshol66@gmail.com>
2023-02-03 14:33:18 +0100

    update

--- front/Form/vues/header.php
+++ front/Form/vues/header.php
@@ -2,6 +2,7 @@
 Html::header("AutoTasks Config", $_SERVER['PHP_SELF'], "config", "plugins");
 ?>
 
-<link href="style.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link rel="stylesheet" href="">
 <link rel="stylesheet" href="trumbowyg/dist/ui/trumbowyg.min.css">
 <div class="autotasks">
